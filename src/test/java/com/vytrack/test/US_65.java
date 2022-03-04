5443ae1342eb57e878310e83c9aa5585e0ea6908
Tamerlan <thuseynov2017@gmail.com>
2022-03-04 13:08:44 -0500

    update US_65.java

--- src/test/java/com/vytrack/test/US_65.java
+++ src/test/java/com/vytrack/test/US_65.java
@@ -1,34 +1,36 @@
 package com.vytrack.test;
-
-import com.vytrack.utilities.WebDriverFactory;
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import com.vytrack.utilities.BrowserUtils;
+import com.vytrack.utilities.VyTrack_Login;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+import static org.testng.Assert.assertEquals;
 
-import static com.vytrack.utilities.ConfigurationReader.getProperty;
-
-public class US_65 {
-    WebDriver driver;
-    @BeforeMethod
-    public void setupMethod(){
-        driver = WebDriverFactory.getDriver(getProperty("browser"));
-        driver.manage().window().maximize();
-        driver.get(getProperty("env"));
-        driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
-
-    }
-    @AfterMethod
-    public void teardown() {
-        driver.close();
-    }
-
-    @Test(dataProvider="VyTrackCredentials",dataProviderClass = DP.class,priority = 1)
+public class US_65 extends DriverLauncher_DP {
+    @Test(dataProvider="VyTrackCredentials",dataProviderClass = DriverLauncher_DP.class,priority = 1)
     public void AC1_OdoCheck(String username,String password){
         VyTrack_Login.login(driver,username,password);
+        Actions action=new Actions(driver);
+        action.moveToElement(driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[2]/a/span"))).click().perform();
+        BrowserUtils.sleep(2);
+        driver.findElement(By.xpath("//span[.='Vehicle Odometer']")).click();
+        BrowserUtils.sleep(4);
+        WebElement errorMessage=driver.findElement(By.xpath("//div[.='You do not have permission to perform this action.']"));
+        BrowserUtils.sleep(4);
+        String actual=errorMessage.getText();
+        String expected="You do not have permission to perform this action.";
+        assertEquals(actual,expected);
     }
 
-
+    @Test(dataProvider ="VyTrackCredentials",dataProviderClass = DriverLauncher_DP.class,priority = 2)
+    public void AC2_OdoCheck(String username,String password){
+        VyTrack_Login.login(driver,username,password);
+    }
+    @Test(dataProvider ="VyTrackCredentials",dataProviderClass = DriverLauncher_DP.class,priority = 3)
+    public void AC3_OdoCheckPage(String username,String password){
+        VyTrack_Login.login(driver,username,password);
+    }
 }
