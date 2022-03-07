30775d6989cafaa8cb59c7d06492b9aae2a89673
Linderwow <alexyiyi92@gmail.com>
2022-03-06 21:08:53 -0500

    First Commit

--- src/test/java/com/vytrack/test/US_58_AutomationForAC1.java
+++ src/test/java/com/vytrack/test/US_58_AutomationForAC1.java
@@ -1,25 +1,25 @@
 package com.vytrack.test;
 
+import com.vytrack.utilities.BrowserUtils;
 import com.vytrack.utilities.VyTrack_Login;
 import com.vytrack.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static com.vytrack.utilities.ConfigurationReader.getProperty;
 
 public class US_58_AutomationForAC1 {
     WebDriver driver;
+
     @BeforeMethod
     public void  setUpMethod(){
-        driver= WebDriverFactory.getDriver(getProperty("browser"));
+        driver = WebDriverFactory.getDriver(getProperty("browser"));
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(getProperty("env"));
@@ -36,19 +36,28 @@ public class US_58_AutomationForAC1 {
                 {"usernameSalesManager1", "password"},
                 {"usernameSalesManager2", "password"},
                 {"usernameSalesManager3", "password"},
-                {"usernameSalesManager4", "password"}
+                {"usernameSalesManager4", "password"},
+
+
         };
     }
 
     @Test(dataProvider = "provider")
     public void VyLogin(String name,String password) {
         VyTrack_Login.login(driver, getProperty(name),getProperty(password));
+       driver.findElement(By.xpath("//li[@class='dropdown dropdown-level-1 first']/following-sibling::li[1]")).click();
+        BrowserUtils.sleep(2);
+        driver.findElement(By.xpath("//span[text()='Vehicle Contracts']")).click();
+        BrowserUtils.sleep(3);
+        String expectedUrl="https://qa2.vytrack.com/entity/Extend_Entity_VehicleContract";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl,expectedUrl);
 
 
     }
-    @AfterMethod
-    public void after(){
+@AfterMethod
+    public  void closeout(){
         driver.close();
-    }
+}
 }
 
--- src/test/java/com/vytrack/test/US_58_AutomationForAC2.java
+++ src/test/java/com/vytrack/test/US_58_AutomationForAC2.java
@@ -1,22 +1,30 @@
 package com.vytrack.test;
 
+import com.vytrack.utilities.BrowserUtils;
 import com.vytrack.utilities.VyTrack_Login;
 import com.vytrack.utilities.WebDriverFactory;
+import com.vytrack.utilities.WebTableUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.security.PublicKey;
 import java.util.concurrent.TimeUnit;
 
 import static com.vytrack.utilities.ConfigurationReader.getProperty;
 
 public class US_58_AutomationForAC2 {
     WebDriver driver;
+
+
+
     @BeforeMethod
     public void  setUpMethod(){
-        driver= WebDriverFactory.getDriver(getProperty("browser"));
+        driver = WebDriverFactory.getDriver(getProperty("browser"));
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(getProperty("env"));
@@ -36,14 +44,21 @@ public class US_58_AutomationForAC2 {
 
     @Test(dataProvider = "provider")
     public void VyLogin(String name,String password) {
-        VyTrack_Login.login(driver, getProperty(name),getProperty(password));
-
+         VyTrack_Login.login(driver, getProperty(name),getProperty(password));
+         driver.findElement(By.xpath("//li[@class='dropdown dropdown-level-1'][1]")).click();
+         WebElement find = driver.findElement(By.xpath("//li[@class='dropdown dropdown-level-1'][1]//*[text()='Vehicle Contracts']"));
+         BrowserUtils.sleep(2);
+         find.click();
+         BrowserUtils.sleep(2);
+         WebElement errorText = driver.findElement(By.xpath("//*[text()='You do not have permission to perform this action.']"));
+         System.out.println(errorText.getText());
+         System.out.println(errorText.isDisplayed());
 
     }
-    @AfterMethod
-    public void after(){
+@AfterMethod
+    public void closeut(){
         driver.close();
-    }
+}
 }
 
 
