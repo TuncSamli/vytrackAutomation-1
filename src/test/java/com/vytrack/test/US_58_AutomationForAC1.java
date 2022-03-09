20b9501d6a2811e1e4c54e57a858c71273706cc4
Alex Akulov <[alexyiyi92@gmail.com]>
2022-03-09 10:51:24 -0500

    last commit

--- src/test/java/com/vytrack/test/US_58_AutomationForAC1.java
+++ src/test/java/com/vytrack/test/US_58_AutomationForAC1.java
@@ -1,22 +1,21 @@
 package com.vytrack.test;
 
 import com.vytrack.utilities.BrowserUtils;
-import com.vytrack.utilities.Driver;
 import com.vytrack.utilities.VyTrack_Login;
-
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.DeviceRotation;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 import static com.vytrack.utilities.ConfigurationReader.getProperty;
-import static com.vytrack.utilities.Driver.closeDriver;
 import static com.vytrack.utilities.Driver.getDriver;
 
 public class US_58_AutomationForAC1 {
@@ -37,7 +36,7 @@ public class US_58_AutomationForAC1 {
     }
 
     @Test(dataProvider = "provider")
-    public void VyLogin(String username, String password) {
+    public void VyLogin(String username, String password) throws Exception {
         VyTrack_Login.login(getDriver(), getProperty(username),getProperty(password));
         getDriver().findElement(By.xpath("//li[@class='dropdown dropdown-level-1 first']/following-sibling::li[1]")).click();
         BrowserUtils.sleep(2);
@@ -53,9 +52,31 @@ public class US_58_AutomationForAC1 {
          System.out.println("Current URL is: " + actualUrl);
 
         System.out.println("Current title has been verified and is: " + actualTitle);
+        screenShot();
 
     }
+    public void screenShot() {
+        try{
+            Robot robot = new Robot();
 
+            //downcast the driver to access TakesScreenshot method
+            TakesScreenshot ts = (TakesScreenshot) getDriver();
+            Rectangle rectangle = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            BufferedImage bufferedImage = robot.createScreenCapture(rectangle);
+            //capture screenshot as output type FILE
+            File file = ts.getScreenshotAs(OutputType.FILE);
+
+
+            //save the screenshot taken in destination path
+            //C:\Users\BitFenix\Desktop
+
+
+            FileUtils.copyFile(file, new File("C:\\Users\\BitFenix\\Desktop\\Screenshot\\Test1_Login.png"));
+        } catch (AWTException| IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("the login page screenshot is taken");
+    }
 }
 
 
--- src/test/java/com/vytrack/test/US_58_AutomationForAC2.java
+++ src/test/java/com/vytrack/test/US_58_AutomationForAC2.java
@@ -5,15 +5,19 @@ import com.vytrack.utilities.Driver;
 import com.vytrack.utilities.VyTrack_Login;
 
 import com.vytrack.utilities.WebTableUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.awt.*;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.security.PublicKey;
 import java.util.concurrent.TimeUnit;
 
@@ -46,8 +50,31 @@ public class US_58_AutomationForAC2 extends TestBase{
         WebElement errorText = getDriver().findElement(By.xpath("//*[text()='You do not have permission to perform this action.']"));
         System.out.println(errorText.getText());
         Assert.assertTrue(errorText.isDisplayed());
+        screenShot();
 
 
+    }
+    public void screenShot() {
+        try{
+            Robot robot = new Robot();
+
+            //downcast the driver to access TakesScreenshot method
+            TakesScreenshot ts = (TakesScreenshot) getDriver();
+            Rectangle rectangle = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            BufferedImage bufferedImage = robot.createScreenCapture(rectangle);
+            //capture screenshot as output type FILE
+            File file = ts.getScreenshotAs(OutputType.FILE);
+
+
+            //save the screenshot taken in destination path
+            //C:\Users\BitFenix\Desktop
+
+
+            FileUtils.copyFile(file, new File("C:\\Users\\BitFenix\\Desktop\\Screenshot\\Test1_Login.png"));
+        } catch (AWTException| IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("the login page screenshot is taken");
     }
 }
 
