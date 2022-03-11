1b28d6799dcba50f4e87389066430d73febcfbe2
Mohammed <95448569+MohammedAlshalchi@users.noreply.github.com>
2022-03-10 20:25:30 -0500

    commit after done

--- src/test/java/com/vytrack/test/US_57.java
+++ src/test/java/com/vytrack/test/US_57.java
@@ -13,66 +13,53 @@ import org.testng.annotations.Test;
 public class US_57 extends TestBase{
 
 
-    @DataProvider(name = "userTruckDriversProvider")
+    @DataProvider(name = "TruckDrivers")
     public Object[][] provideData() {
-        return new Object[][]{
-                {ConfigurationReader.getProperty("usernameTruckDrivers1"), ConfigurationReader.getProperty("password")},
-//                {ConfigurationReader.getProperty("usernameTruckDrivers2"), ConfigurationReader.getProperty("password")},
-//                {ConfigurationReader.getProperty("usernameTruckDrivers3"), ConfigurationReader.getProperty("password")},
-//                {ConfigurationReader.getProperty("usernameTruckDrivers4"), ConfigurationReader.getProperty("password")},
-//                {ConfigurationReader.getProperty("usernameTruckDrivers5"), ConfigurationReader.getProperty("password")},
-//                {ConfigurationReader.getProperty("usernameTruckDrivers6"), ConfigurationReader.getProperty("password")},
-//                {ConfigurationReader.getProperty("usernameTruckDrivers7"), ConfigurationReader.getProperty("password")}
+       return new Object[][]{
+{ConfigurationReader.getProperty("usernameTruckDrivers1"), ConfigurationReader.getProperty("password")},
+
         };
     }
 
-    @DataProvider(name="userStoreManagerProvider")
-    public Object[][] provideData1(){
-        return new Object[][]{
-                {ConfigurationReader.getProperty("usernameStoreManager1"), ConfigurationReader.getProperty("password")},
-//                {ConfigurationReader.getProperty("usernameStoreManager2"), ConfigurationReader.getProperty("password")},
-//                {ConfigurationReader.getProperty("usernameStoreManager3"), ConfigurationReader.getProperty("password")},
-//                {ConfigurationReader.getProperty("usernameStoreManager4"), ConfigurationReader.getProperty("password")},
-//                {ConfigurationReader.getProperty("usernameStoreManager5"), ConfigurationReader.getProperty("password")},
-//                {ConfigurationReader.getProperty("usernameStoreManager6"), ConfigurationReader.getProperty("password")}
-        };
+   @DataProvider(name="StoreManger")
+    public Object[][] provideData1() {
+       return new Object[][]{
+               {ConfigurationReader.getProperty("usernameStoreManager1"), ConfigurationReader.getProperty("password")},
 
-    }
+       };
+   }
 
 
-    @DataProvider(name="userSalesManagerProvider")
+
+   @DataProvider(name="SalesManager")
     public Object[][] provideData2(){
         return new Object[][]{
-                {ConfigurationReader.getProperty("usernameSalesManager1"), ConfigurationReader.getProperty("password")},
-//                {ConfigurationReader.getProperty("usernameSalesManager2"), ConfigurationReader.getProperty("password")},
-//                {ConfigurationReader.getProperty("usernameSalesManager3"), ConfigurationReader.getProperty("password")},
-//                {ConfigurationReader.getProperty("usernameSalesManager4"), ConfigurationReader.getProperty("password")}
+{ConfigurationReader.getProperty("usernameSalesManager1"), ConfigurationReader.getProperty("password")},
 
         };
     }
 
 
-    @Test(dataProvider = "userTruckDriversProvider")
-    public void truckDriverTest (String username,String password){
+
+    @Test(dataProvider = "TruckDrivers")
+    public void vyLogin1 (String username,String password){
       /*  AC1:when users click click the “Learn how to use this space”
-    link on the homepage, users should see:
-    “How To Use Pinbar” and
-    “Use pin icon on the right top corner of the page to create fast
-    access link in the pinbar.”*/
+          link on the homepage, users should see:
+          “How To Use Pinbar” and
+          “Use pin icon on the right top corner of the page to create fast
+           access link in the pinbar.”*/
         VyTrack_Login.login(Driver.getDriver(),username,password);
+
         BrowserUtils.sleep(5);
         WebElement text = Driver.getDriver().findElement(By.xpath("//a[.='Learn how to use this space']"));
         text.click();
         BrowserUtils.sleep(5);
         WebElement textAfterClick1 = Driver.getDriver().findElement(By.xpath("//h3[.='How To Use Pinbar']"));
         WebElement textAfterClick2 = Driver.getDriver().findElement(By.xpath("//p[1]"));
-        // System.out.println(textAfterClick1.getText());
-        // System.out.println(textAfterClick2.getText());
+
         Assert.assertEquals(textAfterClick1.getText(),"How To Use Pinbar");
         Assert.assertEquals(textAfterClick2.getText(),
-                "Use pin icon on the right top corner of page to create fast access link in the pinbar.");
-
-
+ "Use pin icon on the right top corner of page to create fast access link in the pinbar.");
 
 /*
 AC2: users should see an image on the page.
@@ -87,20 +74,86 @@ WebElement picture = Driver.getDriver().findElement(By.xpath("//img[@src='/bundl
         Assert.assertTrue(actual.contains(expected));
         Assert.assertTrue(picture.isDisplayed());
 
-
-
-
-
 }
 
 
 
 
 
+    @Test(dataProvider = "StoreManger")
+    public void vyLogin2 (String username,String password){
+      /*  AC1:when users click click the “Learn how to use this space”
+          link on the homepage, users should see:
+          “How To Use Pinbar” and
+          “Use pin icon on the right top corner of the page to create fast
+           access link in the pinbar.”*/
+        VyTrack_Login.login(Driver.getDriver(),username,password);
 
+        BrowserUtils.sleep(5);
+        WebElement text = Driver.getDriver().findElement(By.xpath("//a[.='Learn how to use this space']"));
+        text.click();
+        BrowserUtils.sleep(5);
+        WebElement textAfterClick1 = Driver.getDriver().findElement(By.xpath("//h3[.='How To Use Pinbar']"));
+        WebElement textAfterClick2 = Driver.getDriver().findElement(By.xpath("//p[1]"));
 
+        Assert.assertEquals(textAfterClick1.getText(),"How To Use Pinbar");
+        Assert.assertEquals(textAfterClick2.getText(),
+                "Use pin icon on the right top corner of page to create fast access link in the pinbar.");
+
+/*
+AC2: users should see an image on the page.
+[in automation testing, just verify the image source.]
+Expected source:
+/bundles/oronavigation/images/pinbar-location.jp
+ */
+
+        WebElement picture = Driver.getDriver().findElement(By.xpath("//img[@src='/bundles/oronavigation/images/pinbar-location.jpg']"));
+        String expected = "/bundles/oronavigation/images/pinbar-location.jp";
+        String actual = picture.getAttribute("src");
+        Assert.assertTrue(actual.contains(expected));
+        Assert.assertTrue(picture.isDisplayed());
 
     }
+
+
+
+    @Test(dataProvider = "TruckDrivers")
+    public void vyLogin3 (String username,String password){
+      /*  AC1:when users click click the “Learn how to use this space”
+          link on the homepage, users should see:
+          “How To Use Pinbar” and
+          “Use pin icon on the right top corner of the page to create fast
+           access link in the pinbar.”*/
+        VyTrack_Login.login(Driver.getDriver(),username,password);
+
+        BrowserUtils.sleep(5);
+        WebElement text = Driver.getDriver().findElement(By.xpath("//a[.='Learn how to use this space']"));
+        text.click();
+        BrowserUtils.sleep(5);
+        WebElement textAfterClick1 = Driver.getDriver().findElement(By.xpath("//h3[.='How To Use Pinbar']"));
+        WebElement textAfterClick2 = Driver.getDriver().findElement(By.xpath("//p[1]"));
+
+        Assert.assertEquals(textAfterClick1.getText(),"How To Use Pinbar");
+        Assert.assertEquals(textAfterClick2.getText(),
+                "Use pin icon on the right top corner of page to create fast access link in the pinbar.");
+
+/*
+AC2: users should see an image on the page.
+[in automation testing, just verify the image source.]
+Expected source:
+/bundles/oronavigation/images/pinbar-location.jp
+ */
+
+        WebElement picture = Driver.getDriver().findElement(By.xpath("//img[@src='/bundles/oronavigation/images/pinbar-location.jpg']"));
+        String expected = "/bundles/oronavigation/images/pinbar-location.jp";
+        String actual = picture.getAttribute("src");
+        Assert.assertTrue(actual.contains(expected));
+        Assert.assertTrue(picture.isDisplayed());
+
+    }
+
+
+}
 
 
 
